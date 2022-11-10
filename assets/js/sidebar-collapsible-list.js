4499d67548f0a0d82716c4c681e55295a5cf2e87
Marc Hennemann <cloud@mhennemann.de>
2022-11-10 22:40:40 +0100

    added dropdown

--- assets/js/sidebar-collapsible-list.js
+++ assets/js/sidebar-collapsible-list.js
@@ -6,13 +6,10 @@ for (i = 0; i < coll.length; i++) {
     
     coll[i].addEventListener("click", function() {
       var parent = this.parentElement;
-      console.log("Parent: ", parent);
       var sidebar_sublist = parent.getElementsByClassName("sidebar-sublist")[0];
 
       
       var list_item = this.getElementsByClassName("sidebar-item");
-
-      console.log("Sub_List: ", sidebar_sublist);
 
       //list_item.classList.toggle("active");
       
@@ -35,12 +32,12 @@ for (i = 0; i < leafs.length; i++) {
   console.log("parents:", leafs[i].parentElement.parentElement);
   leafs[i].addEventListener("click", function() {
       
-      this.classList.add("active");
+      this.classList.toggle("active");
     
       var parent = this.parentElement;
       if ( parent.parentElement.classList.contains("sidebar-sublist") ) {
-        parent.parentElement.add("unfolded");
-        parent.parentElement.style.display = "block";
+        //parent.parentElement.add("unfolded");
+        //parent.parentElement.style.display = "block";
         //parent.parentElement.style.maxHeight = sidebar_sublist.scrollHeight + "px";
       } 
        
