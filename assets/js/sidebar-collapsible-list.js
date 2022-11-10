0702774ad1eb7a62c52fda5cd8e42539b7158275
Marc Henenmann <marc.hennemann@endress.com>
2022-11-10 15:56:39 +0100

    added js

--- assets/js/sidebar-collapsible-list.js
+++ assets/js/sidebar-collapsible-list.js
@@ -3,15 +3,48 @@ var i;
 
 
 for (i = 0; i < coll.length; i++) {
-    console.log(coll[i])
+    
     coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var sidebar_sublist = this.getElementsByClassName("sidebar-sublist")[0];
-      console.log(sidebar_sublist)
+      var parent = this.parentElement;
+      console.log("Parent: ", parent);
+      var sidebar_sublist = parent.getElementsByClassName("sidebar-sublist")[0];
+
+      
+      var list_item = this.getElementsByClassName("sidebar-item");
+
+      console.log("Sub_List: ", sidebar_sublist);
+
+      //list_item.classList.toggle("active");
+      
+      sidebar_sublist.classList.toggle("unfolded");
       if (sidebar_sublist.style.maxHeight){
         sidebar_sublist.style.maxHeight = null;
       } else {
         sidebar_sublist.style.maxHeight = sidebar_sublist.scrollHeight + "px";
       } 
     });
-  }
+}
+
+////////////////////
+// Add Active Link selection on all leave nodes
+var leafs = document.getElementsByClassName("sidebar-leaf");
+var i;
+console.log("LEAFS:", leafs);
+
+for (i = 0; i < leafs.length; i++) {
+  console.log("parents:", leafs[i].parentElement.parentElement);
+  leafs[i].addEventListener("click", function() {
+      
+      this.classList.add("active");
+    
+      var parent = this.parentElement;
+      if ( parent.parentElement.classList.contains("sidebar-sublist") ) {
+        parent.parentElement.add("unfolded");
+        parent.parentElement.style.display = "block";
+        //parent.parentElement.style.maxHeight = sidebar_sublist.scrollHeight + "px";
+      } 
+       
+      
+    });
+}
+
